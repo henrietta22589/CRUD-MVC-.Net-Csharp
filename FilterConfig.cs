12c92776bd9cab54ad3fd2289e6acb36794b5e8c
henrietta22589 <93531231+henrietta22589@users.noreply.github.com>
2022-03-30 22:14:06 +0300

    Add files via upload

---
+++ FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ProJect220022022
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
